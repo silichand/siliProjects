baa83f7b9a5018581c1bc944bd54759d9e5a9b83
Sili Chand <silichand187@gmail.com>
2024-06-10 09:11:55 +0530

    sili projects added

---
+++ arrowFunc.js
@@ -0,0 +1,37 @@
+function getSili () {
+    console.log("sili");
+}
+
+getSili();
+
+const getMili = () => {
+    console.log("mili");
+}
+getMili();
+
+
+
+
+//callbacks
+
+//the function in which the function name is not defined is callback function
+
+// //1st
+// function () {
+//     console.log("function callbacks");
+// }
+// //2nd
+// () => {
+
+// }
+
+// // 3rd syntax
+// (data) => {
+//     console.log(data);
+// }
+// //4th form
+// data => console.log(data);
+// //5th
+// data => {
+//     console.log(data);
+// }
